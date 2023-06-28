9274109dea8e11ff695a7e4ccf139a49a86ecf85
LorensCuscan <lorenscuscan@gmail.com>
2023-06-28 20:24:44 -0300

    User Migration created

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,13 +12,16 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
+            
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->date('birth_date');
+            $table->softDeletes();   
             $table->timestamps();
+            $table->rememberToken();
+            
         });
     }
 
